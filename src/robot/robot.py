952b01e72d57f4a7a3e527d9c6b13a305bff77fb
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-01-21 11:45:22 -0500

    cleaning things up

--- src/robot/robot.py
+++ src/robot/robot.py
@@ -1,15 +1,3 @@
-
-
-
-def importworking ():
-    print("Ca marche")
-
 class Robot:
     def __init__ (self):
-        self.x = 0
-        self.y = 0
-        self.width = 1
-        self.height = 1
-
-    def print (self):
-        print('({0} , {1})'.format(self.x, self.y))
+        self.pos = [0,0]
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,9 +1,8 @@
 from nose.tools import *
 from src.robot.robot import * 
 
-def test_print():
-    allo = Robot()
-    allo.print()
-
+def test_pos():
+    robot = Robot()
+    assert_equal([0,0], robot.pos)
 
 
