789d4c5d9e3d7911085d729b2a87975259f15ddc
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-01-21 20:22:07 -0500

    Ajout de la vitesse au robot

--- src/robot/robot.py
+++ src/robot/robot.py
@@ -1,3 +1,38 @@
+from threading import Thread
+from time import sleep, clock
+
 class Robot:
-    def __init__ (self):
+    def __init__ (self, refresh_time = 10):
         self.pos = [0,0]
+        self.vel = [0,0]
+        self.refresh_time = refresh_time
+        
+    def __update(self, refresh_time):
+        print("Starting Thread for time simulation")
+        while(self.running):
+            time = clock()
+            sleep(refresh_time/1000)
+            time = clock() - time
+            self.pos[0] += self.vel[0] * time
+            self.pos[1] += self.vel[1] * time
+
+
+    def __del__(self):
+        self.running = False
+
+
+    def start(self):
+        self.thread = Thread(target = self.__update, args = (self.refresh_time, ))
+        self.running = True
+        self.thread.setDaemon(True)
+        self.thread.start()
+
+
+    def set_vel(self, vel_x, vel_y):
+        self.vel[0] = vel_x
+        self.vel[1] = vel_y
+
+
+    def move(self, direction, speed):
+        if(direction == 1):
+            self.pos = [self.pos[0] - ticks, self.pos[1]]
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -1,8 +1,26 @@
 from nose.tools import *
 from src.robot.robot import * 
+from time import sleep
 
 def test_pos():
     robot = Robot()
     assert_equal([0,0], robot.pos)
 
 
+def test_speed():
+    robot = Robot()
+    robot.start()
+    sleep(0.1)
+    robot.set_vel(2,1)
+    sleep(0.5)
+
+    x_actual = robot.pos[0]
+    y_actual = robot.pos[1]
+    x_expected = 1.0
+    y_expected = 0.5
+
+    assert_equal_with_error(x_expected, x_actual, 0.05)
+
+def assert_equal_with_error(expected, actual, error_percentage):
+    actual_error = abs(1 - actual/expected)
+    assert_true( actual_error < error_percentage, msg="Error {1}% larger than {0}%".format(round(error_percentage*100, 2), round(actual_error*100,2)))
