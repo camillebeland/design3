01f8754672a4c4eb9fede6898261f69db04cbd8f
camillebeland <camillebeland1@gmail.com>
2016-02-04 15:15:08 -0500

    remove unused import

--- app_images.py
+++ app_images.py
@@ -1,16 +1,12 @@
-import binascii
-import cv2
 from flask import Flask
 from flask.ext.socketio import SocketIO
 from configuration import configuration
-from images.images_provider import Images_Provider
 import base64
 with open("image.jpg", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
 app = Flask(__name__)
 socket_io = SocketIO(app)
-image_provider = Images_Provider()
 
 
 def start_server(port):
