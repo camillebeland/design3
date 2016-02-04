e559b8a7fc91eba3a6e0724960dc0a1bd639fad8
camillebeland <camillebeland1@gmail.com>
2016-02-04 15:17:29 -0500

    split socket for robot and image

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -19,19 +19,20 @@ website.controller('homeController', ['$scope', function ($scope) {
 
     var canvas = document.getElementById("mapCanvas");
     var ctx = canvas.getContext("2d");
-    var socket = io('localhost:5000');
+    var image_socket = io('localhost:5000');
+    var robot_socket = io('localhost:3000');
     ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
 
     ctx.fillStyle = "rgb(200,0,0)";
 
-   setInterval(function(){ socket.emit('fetchPosition') }, 1000); //Refresh data every 1 second
-    socket.on('position', function(msg){
+   setInterval(function(){ robot_socket.emit('fetchPosition') }, 1000); //Refresh data every 1 second
+    robot_socket.on('position', function(msg){
         ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
         ctx.fillRect(msg.robotPosition[0], msg.robotPosition[1], msg.robotPosition[0] + 20, msg.robotPosition[1] + 20);
     });
 
-    setInterval(function(){ socket.emit('fetchImage') }, 2000); //Refresh data every 1 second
-    socket.on('sentImage', function(msg){
+    setInterval(function(){ image_socket.emit('fetchImage') }, 2000); //Refresh data every 1 second
+    image_socket.on('sentImage', function(msg){
         var image = new Image();
         image.src = 'data:image/jpeg;base64,' + msg.image.substring(2,  msg.image.length-1);
         document.body.appendChild(image);
