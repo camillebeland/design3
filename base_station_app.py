c4ea08586412d3ce52217e75e36ab2ee9b0f8b60
camillebeland <camillebeland1@gmail.com>
2016-02-05 18:16:50 -0500

    camera taking pictures

--- base_station_app.py
+++ base_station_app.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask.ext.socketio import SocketIO
 from configuration import configuration
-import base64
-with open("image.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
+from base_station.camera_service import CameraService
+
 
 app = Flask(__name__)
 socket_io = SocketIO(app)
+camera_service = CameraService()
 
 
 @socket_io.on('fetchImage')
 def some_function():
+    encoded_string = camera_service.take_picture_base64()
     socket_io.emit('sentImage',  {'image': str(encoded_string)})
 
 
--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -14,7 +14,7 @@ website.config(function ($stateProvider, $urlRouterProvider) {
 });
 
 
-website.controller('homeController', ['$scope', '$http', function ($scope, $http) {
+website.controller('homeController', ['$scope', '$http', function ($scope) {
 
     window.BASE_STATION_HOST = "localhost:5000";
     window.ROBOT_HOST = "localhost:3000";
@@ -22,32 +22,30 @@ website.controller('homeController', ['$scope', '$http', function ($scope, $http
     var base_station_socket = io(BASE_STATION_HOST);
     var robot_socket = io(ROBOT_HOST);
 
-    this.drawCanvas = function(){
-        var canvas = document.getElementById("mapCanvas");
-        var ctx = canvas.getContext("2d");
+    this.drawCanvas = function() {
+        window.canvas = document.getElementById("mapCanvas");
+        window.ctx = canvas.getContext("2d");
         var base_station_socket = io('localhost:5000');
         ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
         ctx.fillStyle = "rgb(200,0,0)";
+    };
 
-        setInterval(function(){ robot_socket.emit('fetchPosition') }, 1000); //Refresh data every 1 second
-        robot_socket.on('position', function(msg){
-            ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-            ctx.fillRect(msg.robotPosition[0], msg.robotPosition[1], msg.robotPosition[0] + 20, msg.robotPosition[1] + 20);
-        });
+    setInterval(function(){ robot_socket.emit('fetchPosition') }, 1000); //Refresh data every 1 second
+    robot_socket.on('position', function(msg){
+        ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
+        ctx.fillRect(msg.robotPosition[0], msg.robotPosition[1], msg.robotPosition[0] + 20, msg.robotPosition[1] + 20);
+    });
 
-        setInterval(function(){ base_station_socket.emit('fetchImage') }, 2000); //Refresh data every 2 second
-        base_station_socket.on('sentImage', function(msg){
-            var image = new Image();
-            image.src = 'data:image/jpeg;base64,' + msg.image.substring(2,  msg.image.length-1);
+    setInterval(function(){ base_station_socket.emit('fetchImage') }, 2000); //Refresh data every 2 second
+    base_station_socket.on('sentImage', function(msg){
+        var image = new Image();
+        image.src = 'data:image/jpeg;base64,' + msg.image.substring(2,  msg.image.length-1);
 
-            canvas.width = image.width;
-            canvas.height = image.height;
-            ctx.drawImage(image, 0, 0);
-        });
-
-
-        this.drawCanvas();
-    }
+        canvas.width = image.width;
+        canvas.height = image.height;
+        ctx.drawImage(image, 0, 0);
+    });
+    this.drawCanvas();
 }]);
 
 
