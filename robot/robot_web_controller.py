43f0c6d80beda92f84f757676f1d891ece576f7d
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-02-05 13:10:43 -0500

    Fixed bug avec le robot_web_controller

--- robot/robot_web_controller.py
+++ robot/robot_web_controller.py
@@ -1,21 +1,24 @@
 from flask import Flask
 from flask.ext.socketio import SocketIO
 from configuration import configuration
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 socket_io = SocketIO(app)
 
-def inject(robot):
-    robot = robot
+def inject(a_robot):
+    global robot
+    robot = a_robot
 
 def run(port):
     socket_io.run(app, port=port)
 
 
-@socket_io.on('set-velocity')
+@socket_io.on('setVelocity')
 def robot_setvelocity(data):
-    x_velocity = data['x_velocity']
-    y_velocity = data['y_velocity']
+    x_velocity = int(data['x_velocity'])
+    y_velocity = int(data['y_velocity'])
     robot.set_velocity(x_velocity, y_velocity)
 
 
