26626fb6a3745d86ff1cbfb624df10fb2961370d
camillebeland <camillebeland1@gmail.com>
2016-02-07 11:56:29 -0500

    add mock for camera_service

---
+++ base_station/mock_camera_service.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+class MockVideoCamera(object):
+    def __init__(self):
+        image = cv2.imread("base_station/mock_image.jpg", 0)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        self.image_bytes = jpeg.tobytes()
+
+
+    def get_frame(self):
+        return self.image_bytes
+
--- base_station_app.py
+++ base_station_app.py
@@ -1,23 +1,17 @@
-from flask import Flask, Response
-from flask_cors import CORS
-from base_station.camera_service import VideoCamera
 from configuration import configuration
-
-app = Flask(__name__)
-CORS(app)
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+from base_station.camera_service import VideoCamera
+from base_station.mock_camera_service import MockVideoCamera
+from base_station import base_station_web_controller
 
 if __name__ == '__main__':
     config = configuration.getconfig()
-    port = int(config.get('baseapp', 'port'))
-    app.run(port=port)
+
+    port = config.getint('baseapp', 'port')
+    camera_config = config.get('baseapp', 'camera')
+    if camera_config == "webcam":
+        camera = VideoCamera()
+    if camera_config == "mock":
+        camera = MockVideoCamera()
+
+    base_station_web_controller.inject(camera)
+    base_station_web_controller.run(port)
