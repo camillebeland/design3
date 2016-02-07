90b7d5cffff5d27931c09bb2e5f9f492fedbbe3d
camillebeland <camillebeland1@gmail.com>
2016-02-07 12:52:01 -0500

    image in color

--- base_station/mock_camera_service.py
+++ base_station/mock_camera_service.py
@@ -3,7 +3,7 @@ import cv2
 
 class MockVideoCamera(object):
     def __init__(self):
-        image = cv2.imread("base_station/mock_image.jpg", 0)
+        image = cv2.imread("base_station/mock_image.jpg")
         ret, jpeg = cv2.imencode('.jpg', image)
         self.image_bytes = jpeg.tobytes()
 
