5ddd916df248eac6ee549f87888213d4188a9cec
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-02-06 22:15:04 -0500

    petit refactor, web_app.py est juste un main de config et la logique de server http est dans webapp/server.py

---
+++ webapp/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+def run(port):
+    app.run(port=port)
+    
