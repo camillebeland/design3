5635974a05c103170b9693655b02b509bf9cdb43
David Arel <david.arel.1@ulaval.ca>
2016-02-09 15:58:49 -0500

    Moved canvas logic from app.js to canvasController

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -18,46 +18,17 @@ TabEnum = Object.freeze({
 });
 
 website.controller('homeController', ['$scope', '$http', 'Robot', function($scope, $http, Robot) {
-
+  /*Webapp constants*/
   window.BASE_STATION_HOST = "http://localhost:5000";
   window.VIDEO_STREAM = BASE_STATION_HOST + "/video_feed";
   window.ROBOT_HOST = "localhost:3000";
   window.POSITION_REFRESH_TIME_IN_MS = 100
   window.IMAGE_REFRESH_TIME_IN_MS = 5000
 
-  var robot_socket = io(ROBOT_HOST);
-
-  this.initVideoStream = function() {
-    document.getElementById("web-cam-stream").src = VIDEO_STREAM
-  };
-
-  this.drawCanvas = function() {
-    window.canvas = document.getElementById("mapCanvas");
-    window.ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-    ctx.fillStyle = "rgb(200,0,0)";
-  };
-
-  setInterval(function() {
-    robot_socket.emit('fetchPosition')
-  }, POSITION_REFRESH_TIME_IN_MS);
-  robot_socket.on('position', function(msg) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-    var width = 20;
-    var height = 20;
-    ctx.fillRect(msg.robotPosition[0], msg.robotPosition[1], width, height);
-  });
-
-  $scope.send = function(velocity) {
-    robot_socket.emit('setVelocity', velocity);
-  };
-
-  this.init = function() {
+  var init = function() {
     $scope.activeTab = TabEnum.CONTROLS;
-    this.drawCanvas();
-    this.initVideoStream();
   };
 
-  this.init();
+  init();
 
 }]);
---
+++ webapp/static/js/canvasController.js
@@ -0,0 +1,34 @@
+website.controller('canvasController', ['$scope', '$http', 'Robot', function($scope, $http, Robot) {
+
+  var canvas;
+  var canvasContext;
+  var robot_socket = io(ROBOT_HOST);
+
+  var drawCanvas = function() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
+    canvasContext.fillStyle = "rgb(200,0,0)";
+  };
+
+  var initVideoStream = function() {
+    document.getElementById("web-cam-stream").src = VIDEO_STREAM
+  };
+
+  setInterval(function() {
+    robot_socket.emit('fetchPosition')
+  }, POSITION_REFRESH_TIME_IN_MS);
+  robot_socket.on('position', function(msg) {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
+    var width = 20;
+    var height = 20;
+    canvasContext.fillRect(msg.robotPosition[0], msg.robotPosition[1], width, height);
+  });
+
+  function canvasController() {
+    canvas = document.getElementById("mapCanvas");
+    canvasContext = canvas.getContext("2d");
+    drawCanvas();
+    initVideoStream();
+  }
+
+  canvasController();
+}])
