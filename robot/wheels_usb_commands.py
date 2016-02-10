aa438567f83a6bd36f6ddc4f8157f86deaa27f8b
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-02-10 12:06:15 -0500

    Ajout des limites aux commandes usb : On ne peut envoyer d'angle > 180 Les nombres negatifs sont traduit en nombre positif

--- robot/wheels_usb_commands.py
+++ robot/wheels_usb_commands.py
@@ -2,13 +2,31 @@ def stop():
     return "(s)"
 
 def rotate_left(angle):
-    return "(rl{0})".format(chr(angle))
+    return __rotate('l', angle)
 
 def rotate_right(angle):
-    return "(rr{0})".format(chr(angle))
+    return __rotate('r', angle)
+
+def __rotate(direction, angle):
+    assert(abs(angle)<180)
+    return "(r"+direction+"{0})".format(chr(angle))
+
 
 def move_slow(x_pos, y_pos):
-    return "(m{0}{1}{2}{3})".format(chr(x_pos//256), chr(x_pos % 256), chr(y_pos//256), chr(y_pos%256))
+    return __move('m', x_pos, y_pos)
 
 def move(x_pos, y_pos):
-    return "(M{0}{1}{2}{3})".format(chr(x_pos//256), chr(x_pos % 256), chr(y_pos//256), chr(y_pos%256))
+    return __move('M', x_pos, y_pos)
+
+def __move(speed, x_pos, y_pos):
+    x_pos = __apply_limits(x_pos)
+    y_pos = __apply_limits(y_pos)
+    return "("+speed+"{0}{1}{2}{3})".format(chr(x_pos//256), chr(x_pos % 256), chr(y_pos//256), chr(y_pos%256))
+
+def __apply_limits(number):
+    number = min(number, 32767)
+    number = max(number, -32768)
+    if(number < 0):
+        number = -number + 32768
+    return number
+
--- tests/test_usb_wheels_commands.py
+++ tests/test_usb_wheels_commands.py
@@ -16,3 +16,14 @@ def test_move_slow():
 
 def test_move():
     assert_equal("(M\x00\x00\x14\x04)", cmd.move(0, 5124))
+
+@raises(Exception)
+def test_when_rotate_left_of_angle_larger_than_pi_then_raise_exception():
+    cmd.rotate_left(200)
+
+@raises(Exception)
+def test_when_rotate_left_of_a_negative_angle_then_raise_exception():
+    cmd.rotate_left(-45)
+
+def test_when_move_slow_of_negative_value_then_add_bit_sign():
+    assert_equal('(m\x81\x56\x80\x7b)', cmd.move_slow(-342, -123))
