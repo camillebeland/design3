0ad6dff98a9c5b2659a8b3708c401b8373f027a6
David <david.arel@outlook.com>
2016-02-11 21:03:23 -0500

    cleanup and robot now has a circle indicating his direction.

--- webapp/static/js/canvasController.js
+++ webapp/static/js/canvasController.js
@@ -4,27 +4,41 @@ website.controller('canvasController', ['$scope', '$http', 'Robot', function($sc
   var canvasContext;
   var robot_socket = io(ROBOT_HOST);
   var stage;
-  var robot;
+  var completeRobotRepresentation;
 
-  var drawCanvas = function() {
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-    canvasContext.fillStyle = "rgb(200,0,0)";
+  var updateRobot = function(robotData) {
+    completeRobotRepresentation.x = robotData.robotPosition[0];
+    completeRobotRepresentation.y = robotData.robotPosition[1];
+    completeRobotRepresentation.rotation = robotData.robotAngle;
+
+    stage.update();
   };
 
   var initVideoStream = function() {
     document.getElementById("web-cam-stream").src = VIDEO_STREAM
   };
 
-  var initRobot = function(){
-    //canvasContext.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-    var width = 20;
-    var height = 20;
-  //  canvasContext.fillRect(msg.robotPosition[0], msg.robotPosition[1], width, height);
-
+  var initRobot = function() {
     stage = new createjs.Stage("mapCanvas");
-    robot = new createjs.Shape();
-    robot.graphics.beginFill("blue").drawRect(0, 0, width, height);
-    stage.addChild(robot);
+
+    var robotSquareWidth = 30;
+    var robotSquareHeight = 30;
+    var robotSquare = new createjs.Shape();
+    robotSquare.graphics.beginFill("red").drawRect(0, 0, robotSquareWidth, robotSquareHeight);
+    robotSquare.regX = robotSquareWidth / 2;
+    robotSquare.regY = robotSquareHeight / 2;
+
+    var circle = new createjs.Shape();
+    var circleInitialPositionX = 0;
+    var circleInitialPositionY = -robotSquareWidth/2;
+    var circleRadius = 5;
+    circle.graphics.beginFill("black").drawCircle(circleInitialPositionX, circleInitialPositionY, circleRadius);
+
+    completeRobotRepresentation = new createjs.Container();
+    var robotSquareMiddle = robotSquareWidth/2;
+    completeRobotRepresentation.addChild(robotSquare, circle);
+
+    stage.addChild(completeRobotRepresentation);
   }
 
   setInterval(function() {
@@ -32,20 +46,12 @@ website.controller('canvasController', ['$scope', '$http', 'Robot', function($sc
   }, POSITION_REFRESH_TIME_IN_MS);
   robot_socket.on('position', function(msg) {
 
-    //Set position of Shape instance.
-    robot.x = msg.robotPosition[0];
-    robot.y = msg.robotPosition[1];
-    //Add Shape instance to stage display list.
-stage.addChild(robot);
-    //Update stage will render next frame
-    stage.update();
-    console.log("Robot angle: " + msg.robotAngle);
+    updateRobot(msg);
   });
 
   function canvasController() {
     canvas = document.getElementById("mapCanvas");
     canvasContext = canvas.getContext("2d");
-    drawCanvas();
     initVideoStream();
     initRobot();
   }
