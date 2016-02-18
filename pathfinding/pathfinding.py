9c11f59bba9c3f5470539a01cd2b7b7e041a5848
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-02-18 14:53:46 -0500

    merge

--- pathfinding/pathfinding.py
+++ pathfinding/pathfinding.py
@@ -4,3 +4,23 @@ class PathFinder:
     def __init__(self, graph):
         self.__graph = graph
 
+    def find_path(self, from_point, to_point):
+        from_cell = None
+        to_cell = None
+        for cell in self.__graph.nodes():
+            if(cell.contains_point(from_point)):
+                from_cell = cell
+            if(cell.contains_point(to_point)):
+                to_cell = cell
+
+        cell_path = nx.astar_path(self.__graph, from_cell, to_cell)
+        path = list(map(lambda cell : (cell.x, cell.y), cell_path))
+        if(len(path) ==1):
+            path.pop()
+            path.append(to_point)
+        else:
+            path.pop(0)
+            path.pop()
+            path.insert(0,from_point)
+            path.append(to_point)
+        return path
--- robot/robot.py
+++ robot/robot.py
@@ -1,7 +1,11 @@
+from time import sleep
+import math
+
 class Robot():
-    def __init__(self, wheels, worldmap):
+    def __init__(self, wheels, worldmap, pathfinder):
         self.__wheels = wheels
         self.__worldmap = worldmap
+        self.__pathfinder = pathfinder
 
     def move(self, delta_x, delta_y):
         self.__wheels.move(delta_x, delta_y)
@@ -14,3 +18,22 @@ class Robot():
 
     def rotate(self, angle):
         self.__wheels.rotate(angle)
+
+    def move_to(self, final_destination):
+        while(distance(self.getpos(), final_destination) > 1):
+            path = self.__pathfinder.find_path(self.getpos(), final_destination)
+            target = self.find_relative_target(path)
+            self.__wheels.move(target[0], target[1])
+            sleep(0.1)
+
+    def find_relative_target(self, path):
+        if(len(path) == 1):
+            return self.__worldmap.relative_position(path[0])
+        else:
+            return self.__worldmap.relative_position(path[1])
+
+
+def distance(pos1, pos2):
+    x1,y1 = pos1
+    x2,y2 = pos2
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
