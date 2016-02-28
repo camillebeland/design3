e6ffe757f5c38bdcf6f5d94093a6b9dec7e15740
Adam B.-Bolduc <adam.b.bolduc@gmail.com>
2016-02-28 14:40:28 -0500

    refactor de Movement

--- robot_app.py
+++ robot_app.py
@@ -45,9 +45,9 @@ if __name__ == '__main__':
         wheels = WheelsUsbController(serialport,WheelsUsbCommands())
 
     #mesh hardcode
-    cell = Cell(600,400,350,200)
+    cell = Cell(600,400,300,200)
     mesh = Mesh(cell.partitionCells([polygon(200,200,50), polygon(400,200,50), polygon(400,50,50)],10))
     pathfinder = PathFinder(mesh)
-    robot = Robot(wheels, worldmap,pathfinder)
+    robot = Robot(wheels, worldmap, pathfinder)
     robot_web_controller.inject(robot, mesh)
     robot_web_controller.run(host, port)
