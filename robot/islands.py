ee5a38dd7204999ac5b3e44eec1fb6f6c60e2fcf
camillebeland <camillebeland1@gmail.com>
2016-03-06 11:28:26 -0500

    remove comment

--- robot/islands.py
+++ robot/islands.py
@@ -17,12 +17,11 @@ class Islands:
 
     def create_mesh_with_islands(self):
         cell = Cell(960,500,300,200)
-        #mesh = Mesh(cell.partitionCells([polygon(200,200,50), polygon(400,200,50), polygon(400,50,50)],10))
         mesh = Mesh(cell.partitionCells(self.polygons,10))
         return mesh
 
     def __create_polygon_list__(self):
-        self.polygons.extend([polygon(circle['x'], circle['y'], circle['radius']) for circle in self.circles])
-        self.polygons.extend([polygon(pentagon['x'], pentagon['y'], 30) for pentagon in self.pentagons])
-        self.polygons.extend([polygon(square['x'], square['y'], 30) for square in self.squares])
-        self.polygons.extend([polygon(triangle['x'], triangle['y'], 30) for triangle in self.triangles])
+        self.polygons.extend([polygon(circle['x'], circle['y'], circle['radius'] + 20) for circle in self.circles])
+        self.polygons.extend([polygon(pentagon['x'], pentagon['y'], 50) for pentagon in self.pentagons])
+        self.polygons.extend([polygon(square['x'], square['y'], 50) for square in self.squares])
+        self.polygons.extend([polygon(triangle['x'], triangle['y'], 50) for triangle in self.triangles])
