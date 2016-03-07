ee5ccc2bbe4b6333d855ef8b689b76c454466e6e
David <david.arel@outlook.com>
2016-03-06 19:23:53 -0500

    such pep8

--- robot/map.py
+++ robot/map.py
@@ -21,37 +21,37 @@ class Map:
     def __init__(self, width, height):
         self._width = width
         self._height = height
-        self._robotPosition = np.array([width / 2, height / 2])
-        self._robotAngle = 0;
+        self._robot_position = np.array([width / 2, height / 2])
+        self._robot_angle = 0;
 
     def set_robot_position(self, x, y):
         if not self.__is_inside_boundaries(x,y):
             raise Exception("Robot out of map boundaries : {0} , {1}".format(x,y))
         else:
-            self._robotPosition = np.array([x, y])
+            self._robot_position = np.array([x, y])
 
     def set_robot_angle(self, angle):
-        self._robotAngle = angle
+        self._robot_angle = angle
 
     def get_robot_position(self):
-        return self._robotPosition.tolist()
+        return self._robot_position.tolist()
 
     def get_robot_angle(self):
-        return self._robotAngle
+        return self._robot_angle
 
     def get_robot_angle(self):
-        return self._robotAngle
+        return self._robot_angle
 
     def move_robot(self, delta_x, delta_y):
-        delta = rotate_vector(- self._robotAngle, np.array([delta_x, delta_y]))
-        self.set_robot_position(self._robotPosition[0] + delta[0], self._robotPosition[1] + delta[1])
+        delta = rotate_vector(- self._robot_angle, np.array([delta_x, delta_y]))
+        self.set_robot_position(self._robot_position[0] + delta[0], self._robot_position[1] + delta[1])
 
     def rotate_robot(self, angle):
-        self._robotAngle += angle
-        self._robotAngle = self._robotAngle % 360
+        self._robot_angle += angle
+        self._robot_angle = self._robot_angle % 360
 
     def __is_inside_boundaries(self,x, y):
         return x > 0 and x < self._width and y > 0 and y < self._height
 
     def relative_position(self, position):
-        return rotate_vector(self._robotAngle, np.array(position) - np.array(self._robotPosition))
+        return rotate_vector(self._robot_angle, np.array(position) - np.array(self._robot_position))
