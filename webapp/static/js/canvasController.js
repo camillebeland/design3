bf8f5c06b683b755816c4325f585af4e539f46cc
David <david.arel@outlook.com>
2016-03-11 18:06:09 -0500

    Now can toggle islands on or off from UI
    and
    
    Merge commit 'stash' into bug6-logger_fonctionnal_tests
    
    Conflicts:
    	base_station/base_station_web_controller.py
    	robot/map.py
    	robot/movement.py

--- webapp/static/js/canvasController.js
+++ webapp/static/js/canvasController.js
@@ -1,55 +1,59 @@
 website.controller('canvasController', ['$scope', 'RobotService', 'MapService', function($scope, RobotService, MapService) {
 
-  var canvas;
-  var canvasContext;
-  var robot_socket = io(ROBOT_HOST);
-  var stage = new createjs.Stage("mapCanvas");
-  var completeRobotRepresentation;
-  var completeMesh;
+    var canvas;
+    var canvasContext;
+    var robot_socket = io(ROBOT_HOST);
+    var stage = new createjs.Stage("mapCanvas");
+    var completeRobotRepresentation;
+    var completeMesh;
+    var allIslands;
 
-  var updateRobot = function(robotData) {
-    completeRobotRepresentation.x = robotData.robotPosition[0];
-    completeRobotRepresentation.y = canvas.height - robotData.robotPosition[1]; //Because of y axis direction in computer graphics convention
-    completeRobotRepresentation.rotation = robotData.robotAngle;
-  };
+    var updateRobot = function(robotData) {
+        completeRobotRepresentation.x = robotData.robotPosition[0];
+        completeRobotRepresentation.y = canvas.height - robotData.robotPosition[1]; //Because of y axis direction in computer graphics convention
+        completeRobotRepresentation.rotation = robotData.robotAngle;
+    };
 
-  var initVideoStream = function() {
-    var image = new Image();
-    image.src = "http://" + VIDEO_STREAM;
-    var bitmap = new createjs.Bitmap(image);
-    stage.addChild(bitmap);
-  };
+    var initVideoStream = function() {
+        var image = new Image();
+        image.src = "http://" + VIDEO_STREAM;
+        var bitmap = new createjs.Bitmap(image);
+        stage.addChild(bitmap);
+    };
 
-  var initRobot = function() {
-    var robotSquareWidth = 30;
-    var robotSquareHeight = 30;
-    var robotSquare = new createjs.Shape();
-    robotSquare.graphics.beginFill("blue").drawRect(0, 0, robotSquareWidth, robotSquareHeight);
-    robotSquare.regX = robotSquareWidth / 2;
-    robotSquare.regY = robotSquareHeight / 2;
+    var initRobot = function() {
+        var robotSquareWidth = 30;
+        var robotSquareHeight = 30;
+        var robotSquare = new createjs.Shape();
+        robotSquare.graphics.beginFill("blue").drawRect(0, 0, robotSquareWidth, robotSquareHeight);
+        robotSquare.regX = robotSquareWidth / 2;
+        robotSquare.regY = robotSquareHeight / 2;
 
-    var circle = new createjs.Shape();
-    var circleInitialPositionX = 0;
-    var circleInitialPositionY = -robotSquareWidth / 2;
-    var circleRadius = 5;
-    circle.graphics.beginFill("black").drawCircle(circleInitialPositionX, circleInitialPositionY, circleRadius);
+        var circle = new createjs.Shape();
+        var circleInitialPositionX = 0;
+        var circleInitialPositionY = -robotSquareWidth / 2;
+        var circleRadius = 5;
+        circle.graphics.beginFill("black").drawCircle(circleInitialPositionX, circleInitialPositionY, circleRadius);
 
-    completeRobotRepresentation = new createjs.Container();
-    completeRobotRepresentation.addChild(robotSquare, circle);
+        completeRobotRepresentation = new createjs.Container();
+        completeRobotRepresentation.addChild(robotSquare, circle);
 
-    stage.addChild(completeRobotRepresentation);
-  };
+        stage.addChild(completeRobotRepresentation);
+    };
 
-    var initMap = function() {
-        MapService.getMap().then(function(response) {
+    var initIslands = function() {
+        allIslands = new createjs.Container();
+        var whenGetIsComplete = MapService.getMap();
+
+        whenGetIsComplete.then(function(response) {
             for (circle of response.circles) {
                 var island = new createjs.Shape();
                 var circle_x = circle.x;
                 var circle_y = canvas.height - circle.y;
                 var circle_radius = circle.radius;
                 var circle_color = circle.color;
-                island.graphics.beginFill(circle_color).drawCircle(circle_x,circle_y,circle_radius);
-                stage.addChild(island);
+                island.graphics.beginFill(circle_color).drawCircle(circle_x, circle_y, circle_radius);
+                allIslands.addChild(island)
             }
             for (triangle of response.triangles) {
                 var island = new createjs.Shape();
@@ -59,7 +63,7 @@ website.controller('canvasController', ['$scope', 'RobotService', 'MapService', 
                 var triangle_color = triangle.color;
                 var triangle_angle = -90;
                 island.graphics.beginFill(triangle_color).drawPolyStar(triangle_x, triangle_y, triangle_side_length, 3, 0, triangle_angle);
-                stage.addChild(island);
+                allIslands.addChild(island)
             }
             for (pentagon of response.pentagons) {
                 var island = new createjs.Shape();
@@ -69,7 +73,7 @@ website.controller('canvasController', ['$scope', 'RobotService', 'MapService', 
                 var pentagon_color = pentagon.color;
                 var pentagon_angle = -90;
                 island.graphics.beginFill(pentagon_color).drawPolyStar(pentagon_x, pentagon_y, pentagon_side_length, 5, 0, pentagon_angle);
-                stage.addChild(island);
+                allIslands.addChild(island)
             }
             for (square of response.squares) {
                 var island = new createjs.Shape();
@@ -79,69 +83,78 @@ website.controller('canvasController', ['$scope', 'RobotService', 'MapService', 
                 var square_color = square.color;
                 var square_angle = -90;
                 island.graphics.beginFill(square_color).drawPolyStar(square_x, square_y, square_side_length, 4, 0, square_angle);
-                stage.addChild(island);
+                allIslands.addChild(island)
             }
+            stage.addChild(allIslands);
         });
     };
 
-  var initMesh = function() {
-    var whenGetIsComplete = MapService.getMesh();
+    var initMesh = function() {
+        var whenGetIsComplete = MapService.getMesh();
 
-    whenGetIsComplete.then(function(response) {
-      completeMesh = new createjs.Container();
-      for (cell of response.cells) {
-        var square = new createjs.Shape();
-        var rectTopLeftX = cell.x - cell.width / 2;
-        var rectTopLeftY = cell.y + cell.height / 2;
-        square.graphics.beginStroke("black").drawRect(rectTopLeftX, canvas.height - rectTopLeftY, cell.width, cell.height);
-        completeMesh.addChild(square);
-      }
-      stage.addChild(completeMesh);
+        whenGetIsComplete.then(function(response) {
+            completeMesh = new createjs.Container();
+            for (cell of response.cells) {
+                var square = new createjs.Shape();
+                var rectTopLeftX = cell.x - cell.width / 2;
+                var rectTopLeftY = cell.y + cell.height / 2;
+                square.graphics.beginStroke("black").drawRect(rectTopLeftX, canvas.height - rectTopLeftY, cell.width, cell.height);
+                completeMesh.addChild(square);
+            }
+            stage.addChild(completeMesh);
+        });
+    };
+
+
+    $scope.$on('meshToggleOn', function(event) {
+        initMesh();
     });
-  };
 
+    $scope.$on('meshToggleOff', function(event) {
+        stage.removeChild(completeMesh);
+    });
 
-  $scope.$on('meshToggleOn', function(event) {
-    initMesh();
-  });
+    $scope.$on('islandToggleOn', function(event) {
+        initIslands();
+    });
 
-  $scope.$on('meshToggleOff', function(event) {
-    stage.removeChild(completeMesh);
-  });
+    $scope.$on('islandToggleOff', function(event) {
+        stage.removeChild(allIslands);
+    });
 
-  setInterval(function() {
-    robot_socket.emit('fetchPosition');
-  }, POSITION_REFRESH_TIME_IN_MS);
+    setInterval(function() {
+        robot_socket.emit('fetchPosition');
+    }, POSITION_REFRESH_TIME_IN_MS);
 
-  robot_socket.on('position', function(msg) {
-    updateRobot(msg);
-  });
+    robot_socket.on('position', function(msg) {
+        updateRobot(msg);
+    });
 
-  setInterval(function() {
-    stage.update();
-  }, CANVAS_REFRESH_TIME_IN_MS);
+    setInterval(function() {
+        stage.update();
+    }, CANVAS_REFRESH_TIME_IN_MS);
 
-  function canvasController() {
-    canvas = document.getElementById("mapCanvas");
-    canvasContext = canvas.getContext("2d");
-    canvas.height = CANVAS_HEIGHT;
-    canvas.width = CANVAS_WIDTH;
-    initVideoStream();
-      initRobot();
-      initMap();
-      function getMousePos(canvas, evt) {
-          var rect = canvas.getBoundingClientRect();
-          return {
-              x: evt.clientX - rect.left,
-              y: CANVAS_HEIGHT - (evt.clientY - rect.top)
-          };
-      }
+    function canvasController() {
+        canvas = document.getElementById("mapCanvas");
+        canvasContext = canvas.getContext("2d");
+        canvas.height = CANVAS_HEIGHT;
+        canvas.width = CANVAS_WIDTH;
+        initVideoStream();
+        initRobot();
 
-      canvas.addEventListener('mousedown', function(evt) {
-          var mousePos = getMousePos(canvas, evt);
-          RobotService.move_to(mousePos);
-      }, false);
+        function getMousePos(canvas, evt) {
+            var rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: CANVAS_HEIGHT - (evt.clientY - rect.top)
+            };
+        }
 
-  }
+        canvas.addEventListener('mousedown', function(evt) {
+            var mousePos = getMousePos(canvas, evt);
+            RobotService.move_to(mousePos);
+        }, false);
+
+    }
     canvasController();
 }]);
