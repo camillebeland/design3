f45c7517a769c3ca8c94bab8db82d96f748a61a1
camillebeland <camillebeland1@gmail.com>
2016-03-18 18:13:24 -0400

    add space

--- robot/robot_service.py
+++ robot/robot_service.py
@@ -19,7 +19,7 @@ class RobotService:
             try:
                 self.robot_position_from_vision = requests.get(str(self.base_station_address)+ '/robot_position')
             except requests.exceptions.RequestException:
-                print('can\'t fetch robot position from vision' + str(self.base_station_address) + ' is not available')
+                print('can\'t fetch robot position from vision ' + str(self.base_station_address) + ' is not available')
             if not self.robot_position_from_vision.json():
                 self.log_info("Robot position is not found")
             else:
