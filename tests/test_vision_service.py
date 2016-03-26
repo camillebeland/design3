8829c27ef5d5020aef64378c03ab82c0c8b67212
David Arel <david.arel.1@ulaval.ca>
2016-03-26 15:22:59 -0400

    Fixing test

--- tests/test_vision_service.py
+++ tests/test_vision_service.py
@@ -5,7 +5,8 @@ from base_station.vision.table_calibrator import TableCalibrator
 from base_station.vision.treasure_detector import TreasureDetector
 from base_station.vision.robot_detector import RobotDetector
 
-class TestRobotService:
+
+class TestVisionService:
 
     def __init__(self):
         self.camera = MockCameraService(image_path="test_with_islands.jpg")
@@ -13,69 +14,53 @@ class TestRobotService:
         self.treasure_detector = TreasureDetector()
         self.table_calibrator = TableCalibrator()
         self.robot_detector = RobotDetector()
-        self.vision_service = VisionService(self.camera, self.shape_detector, self.treasure_detector, self.table_calibrator, self.robot_detector)
+        self.vision_service = VisionService(self.camera, self.shape_detector, self.treasure_detector,
+                                            self.table_calibrator, self.robot_detector)
         self.vision_service.init_worldmap_contour()
 
-
-    def test_vision_service_with_circles_when_build_map_should_return_map_with_circles_and_polygons(self):
+    def test_vision_service_with_robot_when_find_robot_position_should_return_robot_position(self):
         # Given
-        NB_TRIANGLES = 1
-        NB_CIRCLES = 1
-        NB_SQUARES = 1
-        NB_PENTAGONS = 1
+        ROBOT_POSITION_CENTER = (803.0, 442.375)
+        self.camera_mock = MockCameraService(image_path="test_with_robot.jpg")
+        self.vision_service = VisionService(self.camera_mock, self.shape_detector, self.treasure_detector,
+                                            self.table_calibrator, self.robot_detector)
+        self.vision_service.init_worldmap_contour()
+
+        # When
+        robot_position = self.vision_service.find_robot_position()
+
+        # Then
+        assert robot_position['center'] == ROBOT_POSITION_CENTER
+
+    def test_vision_service_with_no_robot_when_find_robot_position_should_return_nothing(self):
+        # When
+        robot_position = self.vision_service.find_robot_position()
+
+        # Then
+        assert not robot_position
+
+    def test_vision_service_with_treasure_when_find_treasure_should_return_treasure_position(self):
+        # Given
+        NB_TREASURE = 1
+        TREASURE_POSITION_X = 1190
+        TREASURE_POSITION_Y = 237
 
         # When
         map = self.vision_service.build_map()
 
         # Then
-        assert len(map['circles']) == NB_CIRCLES
-        assert len(map['pentagons']) == NB_PENTAGONS
-        assert len(map['triangles']) == NB_TRIANGLES
-        assert len(map['squares']) == NB_SQUARES
-
-    def test_vision_service_with_robot_when_find_robot_position_should_return_robot_position(self):
-        #Given
-        ROBOT_POSITION_CENTER = (803.0, 442.375)
-        self.camera_mock = MockCameraService(image_path="test_with_robot.jpg")
-        self.vision_service = VisionService(self.camera_mock, self.shape_detector, self.treasure_detector, self.table_calibrator, self.robot_detector)
-        self.vision_service.init_worldmap_contour()
-
-        #When
-        robot_position = self.vision_service.find_robot_position()
-
-        #Then
-        assert robot_position['center'] == ROBOT_POSITION_CENTER
-
-    def test_vision_service_with_no_robot_when_find_robot_position_should_return_nothing(self):
-        #When
-        robot_position = self.vision_service.find_robot_position()
-
-        #Then
-        assert not robot_position
-
-    def test_vision_service_with_treasure_when_find_treasure_should_return_treasure_position(self):
-        #Given
-        NB_TREASURE = 1
-        TREASURE_POSITION_X = 1190
-        TREASURE_POSITION_Y = 237
-
-        #When
-        map = self.vision_service.build_map()
-
-        #Then
         assert len(map['treasures']) == NB_TREASURE
         assert map['treasures'][0]['x'] == TREASURE_POSITION_X
         assert map['treasures'][0]['y'] == TREASURE_POSITION_Y
 
-
     def test_vision_service_when_table_calibration_should_return_table_corner_and_ratio(self):
-        #Given
+        # Given
         RATIO = 745.09803921568619
 
-        #When
+        # When
         table_calibration = self.vision_service.get_calibration_data()
 
-        #Then
+        # Then
         assert len(table_calibration['table_contour']) == 4
         assert table_calibration['pixels_per_meter'] == RATIO
 
--- utils/position.py
+++ utils/position.py
@@ -13,5 +13,12 @@ class Position:
     def to_tuple(self):
         return self.x, self.y
 
+    def __eq__(self, other):
+        if other.x == self.x and other.y == self.y:
+            return True
+        else:
+            return False
+
+
     def __str__(self):
         return "Position: x:" + str(self.x) + ", y:" + str(self.y)
