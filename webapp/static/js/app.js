214041e7d06edde7461c22a33deea9bbe8ba3056
David Arel <david.arel.1@ulaval.ca>
2016-03-29 15:31:17 -0400

    minor corrections to check Pull request

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -19,13 +19,13 @@ TabEnum = Object.freeze({
 
 website.controller('homeController', ['$scope', function($scope) {
   /*Webapp constants*/
-  window.BASE_STATION_HOST = "192.168.0.35:5000";
+  window.BASE_STATION_HOST = "localhost:5000";
   window.VIDEO_STREAM = BASE_STATION_HOST + "/video_feed";
-  window.ROBOT_HOST = "192.168.0.34:3000";
-  window.POSITION_REFRESH_TIME_IN_MS = 1000;
-  window.ROBOT_POSITION_FROM_VISION_REFRESH_TIME_IN_MS = 1000;
+  window.ROBOT_HOST = "localhost:3000";
+  window.POSITION_REFRESH_TIME_IN_MS = 100;
+  window.ROBOT_POSITION_FROM_VISION_REFRESH_TIME_IN_MS = 100;
   window.PATH_REFRESH_TIME_IN_MS = POSITION_REFRESH_TIME_IN_MS;
-  window.CANVAS_REFRESH_TIME_IN_MS = 1000;
+  window.CANVAS_REFRESH_TIME_IN_MS = 100;
   window.BACKEND_IMAGE_HEIGHT = 1200;
   window.BACKEND_IMAGE_WIDTH = 1600;
   window.CANVAS_HEIGHT = 600;
