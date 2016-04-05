1da6fcc8eeeeafa1c4a657e39fc963bb4e331e60
Rem0o <rem54321@hotmail.com>
2016-04-05 12:45:21 -0400

    worth

--- Arduino/main/motors.h
+++ Arduino/main/motors.h
@@ -67,7 +67,7 @@ Arduino Pin	Register
 
 #define FREQ 20 //  Hz
 #define DEFAULT_SPEED 1300 // TICKS PER SEC
-#define SLOW_TRIGGER 700
+#define SLOW_TRIGGER 350
 #define MIN_SPEED 300
 #define SLOW_SPEED 750
 #define ROTATE_SPEED 500
