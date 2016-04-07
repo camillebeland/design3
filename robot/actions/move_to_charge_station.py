6578a0cbf30df9d3579ec009c195c7fce6029e71
David Arel <david.arel.1@ulaval.ca>
2016-04-07 19:03:33 -0400

    printed any exception on robot web controller.

--- robot/actions/move_to_charge_station.py
+++ robot/actions/move_to_charge_station.py
@@ -5,13 +5,13 @@ class MoveToChargeStationAction(Action):
 
     def start(self):
         print('Moving To Charge Station')
-        self.__charge_station_angle = -90
+        self.__facing_charge_station_angle = -90
         recharge_station_position = self._context.worldmap.get_recharge_station_position()
         self._context.robot.move_to(recharge_station_position, self.__rotate)
 
     def __rotate(self):
         robot_angle = self._context.worldmap.get_robot_angle()
-        self._context.robot.rotate(self.__charge_station_angle - robot_angle, self.__rotate_done)
+        self._context.robot.rotate(self.__facing_charge_station_angle - robot_angle, self.__rotate_done)
 
     def __rotate_done(self):
         self._context.event_listener.notify_event(self._end_message)
