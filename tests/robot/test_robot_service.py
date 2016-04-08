d61257ad9150cf3dd89269d7ea19584c25577672
David Arel <david.arel.1@ulaval.ca>
2016-04-08 17:35:50 -0400

    fixed test

--- tests/robot/test_robot_service.py
+++ tests/robot/test_robot_service.py
@@ -13,8 +13,9 @@ class TestRobotService:
         code_letter = "A"
         payload = {'code': code_letter}
         mock_response = Mock()
-        expected_response_text = "blue triangle"
-        mock_response.text = expected_response_text
+        expected_response_text_fr = '{"couleur":"bleu"}'
+        expected_response_text_en = {'color', 'blue'}
+        mock_response.text = expected_response_text_fr
         mock_requests_framework.get.return_value = mock_response
 
         # When
@@ -22,7 +23,7 @@ class TestRobotService:
 
         # Then
         mock_requests_framework.get.assert_called_once_with("https://192.168.0.2/", verify=False, params=payload)
-        assert_equals(expected_response_text, response)
+        assert_equals(expected_response_text_en, response)
 
 
 
