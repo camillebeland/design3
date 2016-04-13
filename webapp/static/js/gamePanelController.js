464d0ce280c74b5e86a3adc0c73fc1c06f970cc5
David Arel <david.arel.1@ulaval.ca>
2016-04-12 22:16:26 -0400

    Deleted continue sequence option

--- webapp/static/js/gamePanelController.js
+++ webapp/static/js/gamePanelController.js
@@ -1,7 +1,5 @@
 website.controller('gamePanelController', ['$scope', '$rootScope', 'RobotService', function($scope, $rootScope, robotService) {
 
-    $scope.continueSequence = false;
-
     $scope.actionReadManchester = function() {
         $scope.sequenceToStart = window.actionsEnum.READ_MANCHESTER
     };
@@ -37,21 +35,17 @@ website.controller('gamePanelController', ['$scope', '$rootScope', 'RobotService
     $scope.actionMoveToChargeStation = function(){
         $scope.sequenceToStart = window.actionsEnum.MOVE_TO_CHARGE_STATION
     };
-    
+
     $scope.actionScanTreasures = function(){
         $scope.sequenceToStart = window.actionsEnum.SCAN_TREASURES
     };
-    
+
     $scope.actionsAlignWithChargingStation = function(){
         $scope.sequenceToStart = window.actionsEnum.ALIGN_WITH_CHARGING_STATION
     };
-    
+
     $scope.actionAlignWithTreasure = function(){
         $scope.sequenceToStart = window.actionsEnum.ALIGN_WITH_TREASURE
-    };
-
-    $scope.continueSequenceToggle = function(boolean) {
-        this.continueSequence = boolean;
     };
 
     $scope.startSequence = function() {
