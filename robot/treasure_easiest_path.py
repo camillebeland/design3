c2fd9bb2aaeac6227d2a23197dafd9fe7a1b67c7
David <david.arel@outlook.com>
2016-04-13 20:40:01 -0400

    Changed algorithm to take in consideration the remoteness of islands on treasures.

--- robot/treasure_easiest_path.py
+++ robot/treasure_easiest_path.py
@@ -1,4 +1,5 @@
 from utils.position import Position
+PONDERATION_FACTOR = 10000.0 # 100 pixels = danger
 
 
 class TreasureEasiestPath:
@@ -13,7 +14,6 @@ class TreasureEasiestPath:
         paths = list(map(lambda treasure_position: {'length': self.__find_path_length(position, treasure_position, target_island_position), 'treasure': treasure_position}, treasures_positions))
         return min(paths, key=lambda x: x['length'])['treasure']
 
-
     def __find_path_length(self, robot_position, treasure_position, target_island_position):
         try:
             path_length_to_treasure = len(self.__pathfinder.find_path(robot_position, treasure_position))
@@ -21,8 +21,19 @@ class TreasureEasiestPath:
             path_length = path_length_to_island + path_length_to_treasure
         except:
             path_length = self.__infinity
+
+        path_length += self.__calculate_islands_risk(treasure_position)
         return path_length
 
     def reset_attributes(self, pathfinder, worldmap):
         self.__pathfinder = pathfinder
         self.__worldmap = worldmap
+
+    def __calculate_islands_risk(self, treasure_position):
+        islands = self.__worldmap.worldmap_service.get_islands()
+        islands_positions = list(map(lambda island: Position(island["x"], island["y"]), islands))
+        islands_remoteness = list(map(lambda island_position: island_position.distance(treasure_position), islands_positions))
+        closest_island_distance = min(islands_remoteness)
+        path_ponderation = PONDERATION_FACTOR / closest_island_distance
+        return path_ponderation
+
